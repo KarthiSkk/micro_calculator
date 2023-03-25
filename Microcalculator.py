b68345fdcb579523e99ba96b1701033a19b6005c
KarthiSkk <128567102+KarthiSkk@users.noreply.github.com>
2023-03-25 17:32:36 +0530

    added div

--- Microcalculator.py
+++ Microcalculator.py
@@ -1,8 +1,19 @@
-def sub(a,b):
-    return a-b
-print(sub(a,b))
+def sub(a, b):
+    return a - b
 
 
-def add(a,b):
-    return a+b
-print(add(a,b))
+print(sub(a, b))
+
+
+def add(a, b):
+    return a + b
+
+
+print(add(a, b))
+
+
+def div(a, b):
+    return a / b
+
+
+print(div(a, b))
